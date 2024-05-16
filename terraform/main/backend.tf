68c2913334c7daf50815550d1ada0250d45025ca
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-16 06:41:01 -0300

    added enviroment vars

--- terraform/main/backend.tf
+++ terraform/main/backend.tf
@@ -1,9 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket         = "bucket-cloud-virtually-primarily-rationally-fit-sailfish"
-    key            = "terraform/state"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "dynamo-cloud"
-  }
+  backend "aws" {}
 }
+
