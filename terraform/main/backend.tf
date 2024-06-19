a6f1a2b869a2b5f9a51fcf0001c685ec8341e155
Marco4444 <mscilipoti@itba.edu.ar>
2024-06-19 15:42:04 -0300

    First Deployment using actions II

--- terraform/main/backend.tf
+++ terraform/main/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "bucket2--fairly-completely-equally-main-satyr"
-    key            = "terraform/state"
+    key            = "dev/terraform.state"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "dynamo"
